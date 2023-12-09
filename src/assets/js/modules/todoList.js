b2910fdcf611bbb3296a57f6867e654378ba1e1e
msuzuna <suzuna.matsuo@nijibox.co.jp>
2023-12-09 15:03:26 +0900

    add:追加ボタンのdisabled切り替え機能

--- src/assets/js/modules/todoList.js
+++ src/assets/js/modules/todoList.js
@@ -1,3 +1,20 @@
 export const todoList = () => {
-  console.log("OK");
+  const taskNameInputElement = document.querySelector(
+    '[data-input="taskname"]'
+  );
+  const taskAddElement = document.querySelector('[data-input="addbutton"]');
+  if (!taskNameInputElement && taskAddElement) return;
+
+  const switchAddButtonDisabled = () => {
+    const isInputLength = taskNameInputElement.value.length;
+    if (isInputLength) {
+      taskAddElement.disabled = false;
+    } else {
+      taskAddElement.disabled = true;
+    }
+  };
+
+  taskNameInputElement.addEventListener("input", () => {
+    switchAddButtonDisabled();
+  });
 };
