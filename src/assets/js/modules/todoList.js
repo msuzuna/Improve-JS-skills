07efefa5489ebf4a7184de28aa7b5c847ddca4d4
msuzuna <suzuna.matsuo@nijibox.co.jp>
2023-12-09 22:22:52 +0900

    add:JS Docの追加

--- src/assets/js/modules/todoList.js
+++ src/assets/js/modules/todoList.js
@@ -1,9 +1,21 @@
 export const todoList = () => {
+  /**
+   * @type {HTMLElement}
+   */
   const taskNameInputElement = document.querySelector(
     '[data-input="taskname"]'
   );
+  /**
+   * @type {HTMLElement}
+   */
   const taskDeadlineElement = document.querySelector('[data-input="deadline"]');
+  /**
+   * @type {HTMLElement}
+   */
   const taskAddElement = document.querySelector('[data-input="addbutton"]');
+  /**
+   * @type {HTMLElement}
+   */
   const taskListElement = document.querySelector('[data-taskList="doing"]');
 
   if (
@@ -14,8 +26,15 @@ export const todoList = () => {
   )
     return;
 
+  /**
+   * @type {object[]}
+   */
   let taskDoingArray = [];
 
+  /**
+   * タスク追加ボタンの活性非活性を切り替える関数
+   * @function
+   */
   const switchAddButtonDisabled = () => {
     const isInputLength = taskNameInputElement.value.length;
     if (isInputLength) {
@@ -25,6 +44,10 @@ export const todoList = () => {
     }
   };
 
+  /**
+   * タスクオブジェクトを進行中タスク配列に追加する関数
+   * @function
+   */
   const addTaskObjTotaskDoingArray = () => {
     const taskName = taskNameInputElement.value;
     const taskDeadline = taskDeadlineElement.value
@@ -38,12 +61,20 @@ export const todoList = () => {
     taskDoingArray.push(taskObj);
   };
 
+  /**
+   * タスク入力input要素をリセットする関数
+   * @function
+   */
   const resetTaskInputStatus = () => {
     taskNameInputElement.value = "";
     taskDeadlineElement.value = "";
     taskAddElement.disabled = true;
   };
 
+  /**
+   * 進行中タスク配列の順序を日付の昇降順に入れ替える関数
+   * @function
+   */
   const changeOrderTaskDoingArray = () => {
     const hasDeadlineTaskArray = taskDoingArray.filter((elm) => {
       return elm.taskDeadline !== "none";
@@ -57,6 +88,10 @@ export const todoList = () => {
     taskDoingArray = hasDeadlineTaskArray.concat(noDeadlineTaskArray);
   };
 
+  /**
+   * 進行中タスクリスクの表示を更新する関数
+   * @function
+   */
   const updateTaskListElement = () => {
     while (taskListElement.firstChild) {
       taskListElement.removeChild(taskListElement.firstChild);
