c6db1a2cb6a4fa5041cb57cbac584e570e090052
msuzuna <suzuna.matsuo@nijibox.co.jp>
2023-12-09 15:55:06 +0900

    add:新規作成したタスクを進行中タスクリストへ追加する機能

--- src/assets/js/modules/todoList.js
+++ src/assets/js/modules/todoList.js
@@ -2,8 +2,19 @@ export const todoList = () => {
   const taskNameInputElement = document.querySelector(
     '[data-input="taskname"]'
   );
+  const taskDeadlineElement = document.querySelector('[data-input="deadline"]');
   const taskAddElement = document.querySelector('[data-input="addbutton"]');
-  if (!taskNameInputElement && taskAddElement) return;
+  const taskListElement = document.querySelector('[data-taskList="doing"]');
+
+  if (
+    !taskNameInputElement ||
+    !taskDeadlineElement ||
+    !taskAddElement ||
+    !taskListElement
+  )
+    return;
+
+  const taskDoingArray = [];
 
   const switchAddButtonDisabled = () => {
     const isInputLength = taskNameInputElement.value.length;
@@ -14,7 +25,24 @@ export const todoList = () => {
     }
   };
 
+  const addTaskObjTotaskDoingArray = () => {
+    const taskName = taskNameInputElement.value;
+    const taskDeadline = taskDeadlineElement.value
+      ? new Date(taskDeadlineElement.value)
+      : "none";
+    const taskObj = {
+      taskName,
+      taskDeadline,
+      isCompleted: false,
+    };
+    taskDoingArray.push(taskObj);
+  };
+
   taskNameInputElement.addEventListener("input", () => {
     switchAddButtonDisabled();
+  });
+
+  taskAddElement.addEventListener("click", () => {
+    addTaskObjTotaskDoingArray();
   });
 };
